666133ab3426c7ab83a3b77a140b49559f84c609
Lomakin Sergey <71843430+sirejik@users.noreply.github.com>
2021-01-02 21:56:57 +0700

    Add redirect button to github page

---
+++ src/main/java/org/example/github/action/ConfigureSettingsAction.java
@@ -0,0 +1,12 @@
+package org.example.github.action;
+
+import com.intellij.ide.BrowserUtil;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class ConfigureSettingsAction extends AnAction {
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        BrowserUtil.browse("https://github.com/sirejik");
+    }
+}
